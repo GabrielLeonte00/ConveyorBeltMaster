1ca86fd339d87b31855b4cfd46c49fd4151373f6
Leo <gabriel16.leonte@gmail.com>
2024-06-13 15:59:14 +0300

    Testing pom and module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,21 +4,4 @@ module com.convofig.conveyorbeltmaster {
     requires javafx.web;
     requires javafx.base;
     requires javafx.graphics;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires java.desktop;
-    requires org.apache.poi.poi;
-    requires org.apache.poi.ooxml;
-    requires org.apache.logging.log4j;
-    requires commons.math3;
-
-    opens com.convofig.conveyorbeltmaster to javafx.fxml;
-    exports com.convofig.conveyorbeltmaster;
-    exports com.convofig.controllers;
-    opens com.convofig.controllers to javafx.fxml;
 }
